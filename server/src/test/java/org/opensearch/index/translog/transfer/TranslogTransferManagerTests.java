6f078b637dd9b2c2f6ec9f85baf5e2161729ed34
Raghuvansh Raj <raghraaj@amazon.com>
2023-04-06 09:26:26 +0530

    Add multipart upload integration for translog and segment files
    Signed-off-by: Raghuvansh Raj <raghraaj@amazon.com>

--- server/src/test/java/org/opensearch/index/translog/transfer/TranslogTransferManagerTests.java
+++ server/src/test/java/org/opensearch/index/translog/transfer/TranslogTransferManagerTests.java
@@ -36,7 +36,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
@@ -73,26 +74,24 @@ public class TranslogTransferManagerTests extends OpenSearchTestCase {
     }
 
     @SuppressWarnings("unchecked")
-    public void testTransferSnapshot() throws IOException {
+    public void testTransferSnapshot() throws Exception {
         AtomicInteger fileTransferSucceeded = new AtomicInteger();
         AtomicInteger fileTransferFailed = new AtomicInteger();
         AtomicInteger translogTransferSucceeded = new AtomicInteger();
         AtomicInteger translogTransferFailed = new AtomicInteger();
 
         doNothing().when(transferService)
-            .uploadBlob(any(TransferFileSnapshot.class), Mockito.eq(remoteBaseTransferPath.add(String.valueOf(primaryTerm))), any());
-        doAnswer(invocationOnMock -> {
-            ActionListener<TransferFileSnapshot> listener = (ActionListener<TransferFileSnapshot>) invocationOnMock.getArguments()[3];
-            listener.onResponse((TransferFileSnapshot) invocationOnMock.getArguments()[1]);
-            return null;
-        }).when(transferService)
-            .uploadBlobByThreadPool(
-                anyString(),
+            .uploadBlob(
                 any(TransferFileSnapshot.class),
-                any(BlobPath.class),
-                any(ActionListener.class),
+                Mockito.eq(remoteBaseTransferPath.add(String.valueOf(primaryTerm))),
                 any(WritePriority.class)
             );
+        doAnswer(invocationOnMock -> {
+            ActionListener<TransferFileSnapshot> listener = (ActionListener<TransferFileSnapshot>) invocationOnMock.getArguments()[2];
+            Set<TransferFileSnapshot> transferFileSnapshots = (Set<TransferFileSnapshot>) invocationOnMock.getArguments()[0];
+            transferFileSnapshots.forEach(listener::onResponse);
+            return null;
+        }).when(transferService).uploadBlobs(anySet(), anyMap(), any(ActionListener.class), any(WritePriority.class));
 
         FileTransferTracker fileTransferTracker = new FileTransferTracker(new ShardId("index", "indexUUid", 0)) {
             @Override
