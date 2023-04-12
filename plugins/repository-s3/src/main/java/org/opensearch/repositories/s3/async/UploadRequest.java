e5cfb5eaa7a4b0c7cdd9682e05b2dd4e9ec8531d
Raghuvansh Raj <raghraaj@amazon.com>
2023-04-12 12:58:21 +0530

    ongoing

---
+++ plugins/repository-s3/src/main/java/org/opensearch/repositories/s3/async/UploadRequest.java
@@ -0,0 +1,61 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.repositories.s3.async;
+
+import org.opensearch.common.blobstore.stream.write.WritePriority;
+import org.opensearch.common.blobstore.transfer.UploadFinalizer;
+
+public class UploadRequest {
+    private final String bucket;
+    private final String key;
+    private final long contentLength;
+    private final long checksum;
+    private final WritePriority writePriority;
+    private final UploadFinalizer uploadFinalizer;
+
+    public UploadRequest(
+        String bucket,
+        String key,
+        long contentLength,
+        long checksum,
+        WritePriority writePriority,
+        UploadFinalizer uploadFinalizer
+    ) {
+        this.bucket = bucket;
+        this.key = key;
+        this.contentLength = contentLength;
+        this.checksum = checksum;
+        this.writePriority = writePriority;
+        this.uploadFinalizer = uploadFinalizer;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public long getContentLength() {
+        return contentLength;
+    }
+
+    public long getChecksum() {
+        return checksum;
+    }
+
+    public WritePriority getWritePriority() {
+        return writePriority;
+    }
+
+    public UploadFinalizer getUploadFinalizer() {
+        return uploadFinalizer;
+    }
+}
